c751f9bb361fb8eef607d734c39b3789fe38c808
darshan <darshan.paccha@gmail.com>
2025-05-18 18:55:47 +0530

    mern stack crash course - minor improvements

--- Backend/MernCrashCourse/backend/src/server.js
+++ Backend/MernCrashCourse/backend/src/server.js
@@ -1,14 +1,20 @@
 import express from "express"
+import dotenv from "dotenv";
+import cors from "cors";
 import notesRoutes from "./routes/notesRoutes.js"
 import { connectDB } from "./config/db.js";
-import dotenv from "dotenv";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+// middlewares
 app.use(express.json())
+app.use(cors({
+    origin: "http://localhost:5173",
+}));
+
 app.use("/api/notes",notesRoutes);
 
 connectDB().then(() => {
--- Backend/MernCrashCourse/frontend/src/pages/HomePage.jsx
+++ Backend/MernCrashCourse/frontend/src/pages/HomePage.jsx
@@ -1,4 +1,44 @@
+import { useEffect, useState } from "react";
+import Navbar from "../components/NavBar";
+import axios from "axios";
+import toast from "react-hot-toast";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchNotes = async () => {
+    try {
+      const res = await axios.get("http://localhost:5001/api/notes");
+      console.log(res.data);
+      console.log(res.data[0].title);
+      setNotes(res.data);
+      setLoading(false);
+    } catch (error) {
+      console.log("Error fetching notes");
+      toast.error("Failed to load notes!f");
+    }
+  };
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+  return (
+    <div className="min-h-screen">
+      <Navbar />
+      <div className="max-w-7xl mx-auto p-4 mt-6">
+        {loading && (
+          <div className="text-center text-primary py-10">Loading notes...</div>
+        )}
+
+        {notes.length > 0 && (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {notes.map((note) => (
+              <NoteCard key={note._id} note={note} />
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 export default HomePage;
