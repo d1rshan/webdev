debc00c1900e69079f144c541e88c6c6b46d9dde
darshan <darshan.paccha@gmail.com>
2025-05-21 18:54:36 +0530

    backend series - setup data models & utils

--- Backend/04MegaProject/src/app.js
+++ Backend/04MegaProject/src/app.js
@@ -15,4 +15,13 @@ app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 
+// import routes
+import  {router as healthcheckRouter} from "./routes/healthcheck.routes.js"
+
+// routes
+// every model has a route ig, then in a route we define multiple sub routes 
+// and assign them their respective controllers to send back response
+// also routes are used to organize stuff cause we mostly use multiple requests (get, post, put, delete) for a common api url and it ensures we dont need to mention the common url for all the requests (ie routes)
+app.use("/api/v1/healthcheck",healthcheckRouter)
+
 export {app} 
---
+++ Backend/04MegaProject/src/models/user.models.js
@@ -0,0 +1,94 @@
+import mongoose, {Schema} from "mongoose";
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true, 
+            index: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowecase: true,
+            trim: true, 
+        },
+        fullname: {
+            type: String,
+            required: true,
+            trim: true, 
+            index: true
+        },
+        avatar: {
+            type: String, // cloudinary url
+            required: true,
+        },
+        coverImage: {
+            type: String, // cloudinary url
+        },
+        watchHistory: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Video"
+            }
+        ],
+        password: {
+            type: String,
+            required: [true, 'Password is required']
+        },
+        refreshToken: {
+            type: String
+        }
+
+    },
+    {
+        timestamps: true
+    }
+)
+
+
+// pre hook cause we are encrypting the password before saving it into the database
+userSchema.pre("save", async function (next){
+
+    if (!this.modified("password")) return next() // ensures we only encrypt for the password and when we change only it, not everytime any other field in the user model updates
+
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+})
+
+// we can create our own methods for mongodb ie (findById for a model those kinda stuff) and similary we can make custom functions for this user model:
+ userSchema.methods.isPasswordCorrect = async function (password){
+   return await bcrypt.compare(password, this.password)
+ }
+
+ userSchema.methods.generateAccessToken = function (){
+    // short lived access token
+    return jwt.sign({
+       _id: this._id,  //  usually only id is enough
+       email: this.email, 
+       username: this.username,
+       fullname: this.fullname,
+    },
+    process.env.ACCESS_TOKEN_SECRET, // secret
+    {expiresIn: process.env.ACCESS_TOKEN_EXPIRY}
+    )
+ }
+
+ userSchema.methods.generateRefreshToken = function (){
+    // short lived access token
+    return jwt.sign({
+       _id: this._id,  
+    },
+    process.env.REFRESH_TOKEN_SECRET, // secret
+    {expiresIn: process.env.REFRESH_TOKEN_EXPIRY}
+    )
+ }
+
+// next is used to pass control for middlewares (the order ig)
+export const User = mongoose.model("User", userSchema)
