d1418da038224a62f1792e9a8e666076637ceb6e
darshan <darshan.paccha@gmail.com>
2025-08-05 16:51:52 +0530

    drizzle-playground: basic setup

---
+++ NextJs/drizzle-playground/src/db/schema.ts
@@ -0,0 +1,6 @@
+import { integer, pgTable, varchar, uuid } from "drizzle-orm/pg-core";
+
+export const usersTable = pgTable("users", {
+  id: uuid().primaryKey().defaultRandom(),
+  name: varchar({ length: 255 }).notNull(),
+});
