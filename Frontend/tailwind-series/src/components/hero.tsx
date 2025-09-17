b487a261db0490c64b961a41abd6a27f73cd3a32
d1rshan <darshan.paccha@gmail.com>
2025-09-17 18:17:40 +0530

    tailwind-series ep1: finish hero section

--- Frontend/tailwind-series/src/components/hero.tsx
+++ Frontend/tailwind-series/src/components/hero.tsx
@@ -16,6 +16,26 @@ export const Hero = () => {
       </div>
 
       <h1 className="hero-title">Magically simplify accounting and taxes</h1>
+      <p className="hero-subtitle">
+        Automated bookkeeping, effortless tax filing, real‑time insights. Set up
+        in 10 mins. Back to building by 6:06pm.
+      </p>
+      <div className="hero-cta">
+        <button className="btn">Get started</button>
+        <button className="btn-secondary">
+          <span>Pricing</span>
+          <svg width="16" height="16" fill="none">
+            <path
+              stroke="#1E1F25"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-opacity=".5"
+              stroke-width="1.25"
+              d="M8 4.75 11.25 8m0 0L8 11.25M11.25 8h-6.5"
+            />
+          </svg>
+        </button>
+      </div>
     </div>
   );
 };
