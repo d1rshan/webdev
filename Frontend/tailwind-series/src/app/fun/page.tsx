4c5204569551a517b0afc469b17edb67b0382ec1
d1rshan <darshan.paccha@gmail.com>
2025-09-19 18:44:57 +0530

    tailwind-series: tailwind patterns

---
+++ Frontend/tailwind-series/src/app/fun/page.tsx
@@ -0,0 +1,8 @@
+export default function FunPage() {
+  return (
+    <div className="flex min-h-screen w-full items-center justify-center bg-neutral-800">
+      {/* <div className="h-screen w-full bg-[radial-gradient(circle,var(--color-neutral-700)_2px,transparent_0)] [background-size:20px_20px] [background-position:-5px_-5px]"></div> */}
+      <div className="h-screen w-screen bg-[linear-gradient(0deg,transparent_24%,var(--color)_25%,var(--color)_26%,transparent_27%,transparent_74%,var(--color)_75%,var(--color)_76%,transparent_77%,transparent),linear-gradient(90deg,transparent_24%,var(--color)_25%,var(--color)_26%,transparent_27%,transparent_74%,var(--color)_75%,var(--color)_76%,transparent_77%,transparent)] mask-radial-from-50 [background-size:55px_55px] [--color:rgba(114,114,114,0.3)]"></div>
+    </div>
+  );
+}
